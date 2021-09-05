1cafa08ef9ff78d9773aa11ce28c4cbeb46827b1
zhouhongyin <zhouhongyin@msunsoft.com>
2021-09-05 16:56:04 +0800

    pop1-commit1

---
+++ src/main/java/com/git/entity/People.java
@@ -0,0 +1,14 @@
+package com.git.entity;
+
+/**
+ * @description:
+ * @author: zhouhongyin
+ * @time: 2021/9/5 16:51
+ */
+public class People {
+
+    private String name;
+
+    private int age;
+
+}
