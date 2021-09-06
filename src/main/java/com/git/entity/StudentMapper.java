1e49ea8bf65c1d3bea1b9a76e7355f3775df8677
zhouhongyin <zhouhongyin@msunsoft.com>
2021-09-06 08:43:16 +0800

    zhy1-commit1

---
+++ src/main/java/com/git/entity/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.git.entity;
+
+/**
+ * @description:
+ * @author: zhouhongyin
+ * @time: 2021/9/5 17:37
+ */
+public interface StudentMapper {
+
+    void selectStu();
+
+    void aaaaa();
+
+
+
+
+}
