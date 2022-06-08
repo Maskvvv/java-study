8bbbaafb53d2b49c1f6559228ba6085fd8a2b60d
zhouhongyin <1429855087@qq.com>
2022-06-08 10:03:31 +0800

    文件上传

--- src/main/java/com/zhy/java基础/IO/FileController.java
+++ src/main/java/com/zhy/java基础/IO/FileController.java
@@ -3,11 +3,15 @@ package com.zhy.java基础.IO;
 import com.zhy.java基础.IO.zip.ZipUtil;
 import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.midi.Soundbank;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -46,5 +50,14 @@ public class FileController {
 
         IOUtils.closeQuietly(outputStream);
     }
+
+    @PostMapping("upload")
+    public void uploadFile(HttpServletRequest request, MultipartFile file) throws IOException {
+        System.out.println(file.getName());
+        System.out.println(file.getOriginalFilename());
+        System.out.println(file.getSize());
+        System.out.println(file.getContentType());
+    }
+
 
 }
