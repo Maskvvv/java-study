8d47b0576af0ffe68daf10c98c90dc3258b71614
zhouhongyin <1429855087@qq.com>
2022-07-04 11:55:46 +0800

    Content-Disposition

--- src/main/java/com/zhy/web/header/HeaderController.java
+++ src/main/java/com/zhy/web/header/HeaderController.java
@@ -2,15 +2,19 @@ package com.zhy.web.header;
 
 import cn.hutool.core.io.IoUtil;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.activation.MimeType;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author zhouhongyin
@@ -26,8 +30,10 @@ public class HeaderController {
 
         //response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=a.pdf");
         //response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "inline;filename=a.pdf");
-        //response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "application/octet-stream;filename="+ new String("富 雨的 简历1653869894.pdf".getBytes(), "ISO8859-1"));
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename="+ new String("富 雨的 简历1653869894.pdf".getBytes(), "ISO8859-1"));
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "inline;filename="+ new String("富 雨的 简历1653869894.pdf".getBytes(), "ISO8859-1"));
+
+
+        //response.setHeader(HttpHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE);
         response.setHeader(HttpHeaders.CONTENT_TYPE, "application/pdf");
 
         FileInputStream fileInputStream = new FileInputStream("D:\\UserFiles\\桌面\\富雨的简历1653869894.pdf");
@@ -49,6 +55,22 @@ public class HeaderController {
 
         IoUtil.close(outputStream);
         IoUtil.close(fileInputStream);
+    }
+
+    @GetMapping("content/disposition/html")
+    public void contentDispositionHtml(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletOutputStream outputStream = response.getOutputStream();
+
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=a.html");
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_HTML_VALUE);
+
+        String htmlContent = "<h1> 我是标题</h1>";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(htmlContent.getBytes(StandardCharsets.UTF_8));
+
+        IoUtil.copy(byteArrayInputStream, outputStream);
+
+        IoUtil.close(outputStream);
+        IoUtil.close(byteArrayInputStream);
     }
 
 
