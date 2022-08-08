0e3bea0dd203bb9c36d1ea9e106505125e987a4c
zhouhongyin <1429855087@qq.com>
2022-08-08 13:59:17 +0800

    DoubleTest

---
+++ src/main/java/com/zhy/常用java类/mydouble/DoubleTest.java
@@ -0,0 +1,17 @@
+package com.zhy.常用java类.mydouble;
+
+import org.junit.Test;
+
+/**
+ * @author zhouhongyin
+ * @since 2022/8/1 9:22
+ */
+public class DoubleTest {
+
+    @Test
+    public void doubleTest1() {
+        System.out.println(Double.compare(1.00, 1.0) == 0);
+
+    }
+
+}
