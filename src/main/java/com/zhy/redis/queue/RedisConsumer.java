f31be88f7be10f3b90532def1e795a974f684010
zhouhongyin <1429855087@qq.com>
2022-08-18 15:18:28 +0800

    redis queue test

--- src/main/java/com/zhy/redis/queue/RedisConsumer.java
+++ src/main/java/com/zhy/redis/queue/RedisConsumer.java
@@ -39,7 +39,7 @@ public class RedisConsumer {
                 }
 
             } catch (Exception e) {
-                log.error(e.getMessage());
+                //log.error(e.getMessage());
             }
         }
 
