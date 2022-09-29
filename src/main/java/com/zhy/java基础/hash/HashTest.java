6746679a166a48bbd3605865bb234740298cf5f3
zhouhongyin <1429855087@qq.com>
2022-09-29 13:56:51 +0800

    hash

---
+++ src/main/java/com/zhy/java基础/hash/HashTest.java
@@ -0,0 +1,46 @@
+package com.zhy.java基础.hash;
+
+import org.junit.Test;
+
+/**
+ * @author zhouhongyin
+ * @since 2022/9/27 10:16
+ */
+public class HashTest {
+
+    @Test
+    public void hashTest() {
+
+        String key = "ssfasdfajkfjagkajlsjgkfjal;ksdflskgjasdfalkssdfasdg";
+
+        System.out.println(key.hashCode());
+
+
+        int h;
+        int hash = (h = key.hashCode()) ^ (h >>> 16);
+        System.out.println(hash);
+
+        int length = 16;
+        System.out.println(hash & (length - 1));
+        System.out.println(hash % (length - 1));
+
+    }
+
+    @Test
+    public void hashTest1() {
+
+        String key = "glskadjflakjf52asdkjflasdjglshgjklahsgalshdlfkasdhflaksdfhasldhgjhoiirutwi" +
+                "oeurq093457298345-1231;lgsdfglks;l&()*^^%^&#^_:nasfhworeLKjlksdjf;ladgya9oiuwrwerwerwerkas";
+
+        char[] chars = key.toCharArray();
+
+        int result = 1;
+        for (char cha : chars) {
+            result = 31 * result + cha;
+        }
+        System.out.println(result);
+
+        System.out.println(key.hashCode());
+    }
+
+}
