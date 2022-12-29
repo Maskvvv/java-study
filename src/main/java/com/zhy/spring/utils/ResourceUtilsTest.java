16bb2ddc69b1ae2c8dc1c761bb676202576b2d86
zhouhongyin <1429855087@qq.com>
2022-12-29 11:41:25 +0800

    spring utils

---
+++ src/main/java/com/zhy/spring/utils/ResourceUtilsTest.java
@@ -0,0 +1,17 @@
+package com.zhy.spring.utils;
+
+import org.junit.Test;
+import org.springframework.util.ResourceUtils;
+
+/**
+ * @author zhouhongyin
+ * @since 2022/12/28 10:53
+ */
+public class ResourceUtilsTest {
+
+    @Test
+    public void test1() {
+        ResourceUtils.getFile()
+    }
+
+}
