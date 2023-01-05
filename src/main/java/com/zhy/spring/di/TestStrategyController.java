05002d35deeafbee5628530ef402141439fb2a7e
zhouhongyin <1429855087@qq.com>
2023-01-05 14:12:19 +0800

    autowired

--- src/main/java/com/zhy/spring/di/TestStrategyController.java
+++ src/main/java/com/zhy/spring/di/TestStrategyController.java
@@ -1,5 +1,6 @@
 package com.zhy.spring.di;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,19 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author zhouhongyin
  * @since 2022/12/12 14:31
  */
+@Slf4j
 @RestController
 @RequestMapping("test_strategy")
 public class TestStrategyController {
     private final Map<TestStrategyEunm, TestStrategy> strategyMap;
     private final List<TestStrategy> strategyList;
+    private final Map<String, TestStrategy> strategyDefaultMap;
 
     @Autowired
-    public TestStrategyController(List<TestStrategy> strategyList) {
+    public TestStrategyController(List<TestStrategy> strategyList, Map<String, TestStrategy> strategyDefaultMap) {
         this.strategyList = strategyList;
         this.strategyMap = new ConcurrentHashMap<>();
+        this.strategyDefaultMap = strategyDefaultMap;
         init();
     }
 
@@ -38,6 +42,10 @@ public class TestStrategyController {
         for (TestStrategy testStrategy : strategyList) {
             System.out.println(testStrategy.getStrategyEunm().getName());
         }
+
+        strategyDefaultMap.forEach((key, value) -> {
+            log.info("strategyDefaultMap: {}-{}", key, value);
+        });
 
         //Object strategy = strategyList.stream().filter(s -> s.getStrategyEunm().equals(strategyEunm)).findFirst().orElse(null).strategy();
         //return (String) strategy;
