99c2d902d9e1f485f536af06e9515340ebf85d7c
zhouhongyin <1429855087@qq.com>
2023-02-08 16:01:10 +0800

    jmap profile GC

---
+++ src/main/java/com/zhy/java基础/jvm/GCTest.java
@@ -0,0 +1,24 @@
+package com.zhy.java基础.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class GCTest {
+    public static void main(String[] args) {
+        List<Data> list = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            list.add(new Data());
+        }
+        try {
+            // 程序sleep 10分钟
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Data {
+    byte[] array1 = new byte[1024 * 1024]; //1m
+}
