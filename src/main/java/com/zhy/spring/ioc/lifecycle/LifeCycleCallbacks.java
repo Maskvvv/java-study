7ad8d8d136ce85b4136779707cd0e533ae39ddc9
zhouhongyin <1429855087@qq.com>
2023-02-13 14:28:08 +0800

    initialization' s callback afterPropertiesSet

--- src/main/java/com/zhy/spring/ioc/lifecycle/LifeCycleCallbacks.java
+++ src/main/java/com/zhy/spring/ioc/lifecycle/LifeCycleCallbacks.java
@@ -1,20 +1,23 @@
 package com.zhy.spring.ioc.lifecycle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 /**
+ * bean 的初始化和销毁
  *
  * @author zhouhongyin
  * @since 2023/1/3 15:49
  */
+@Slf4j
 @Component
 public class LifeCycleCallbacks implements InitializingBean, DisposableBean {
 
 
     /**
-     * 在 spring 容器设置完 bean 的必要属性后会执行改方法
+     * 在 spring 容器设置完当前 bean 的必要属性后会执行改方法
      * initialization work after the container has set all necessary properties on the bean
      *
      * 不建议使用，因为这样会把代码耦合金spring。建议用 @PostConstruct 或者 @Bean 的 initMethod 属性代替
@@ -23,6 +26,7 @@ public class LifeCycleCallbacks implements InitializingBean, DisposableBean {
      */
     @Override
     public void afterPropertiesSet() throws Exception {
+        log.info("LifeCycleCallbacks-afterPropertiesSet");
 
     }
 
