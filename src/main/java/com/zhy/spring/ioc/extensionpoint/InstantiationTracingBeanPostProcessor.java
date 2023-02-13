cc784e1a7456262855976b74cef632cfd86fabd5
zhouhongyin <1429855087@qq.com>
2023-02-13 11:01:19 +0800

    BeanPostProcessor and Reflection

--- src/main/java/com/zhy/spring/ioc/extensionpoint/InstantiationTracingBeanPostProcessor.java
+++ src/main/java/com/zhy/spring/ioc/extensionpoint/InstantiationTracingBeanPostProcessor.java
@@ -1,11 +1,14 @@
 package com.zhy.spring.ioc.extensionpoint;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
 
 /**
  * spring example: AutowiredAnnotationBeanPostProcessor
@@ -21,12 +24,19 @@ public class InstantiationTracingBeanPostProcessor implements BeanPostProcessor,
         return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
     }
 
+    @SneakyThrows
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 
-        if (bean instanceof BeanPostProcessorBean) {
+        if (bean instanceof BeanPostProcessorBean processorBean) {
             log.info("BeanPostProcessorBean-postProcessAfterInitialization:" + "Bean '" + beanName + "' created : " + bean.toString());
+
+            Class<? extends BeanPostProcessorBean> processorBeanClass = processorBean.getClass();
+            Field nameFiled = processorBeanClass.getDeclaredField("name");
+            nameFiled.setAccessible(true);
+            nameFiled.set(processorBean, "postProcessAfterInitialization");
         }
+
         return bean;
     }
 
