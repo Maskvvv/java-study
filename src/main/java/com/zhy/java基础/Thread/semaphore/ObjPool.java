a08b6f1e55c59e10038701bc8742606a31b6b86b
zhouhongyin <1429855087@qq.com>
2023-02-15 15:34:22 +0800

    semaphore implement limiter

---
+++ src/main/java/com/zhy/java基础/Thread/semaphore/ObjPool.java
@@ -0,0 +1,45 @@
+package com.zhy.java基础.Thread.semaphore;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.Semaphore;
+import java.util.function.Function;
+
+class ObjPool<T, R> implements {
+    final List<T> pool;
+    // 用信号量实现限流器
+    final Semaphore sem;
+
+    // 构造函数
+    ObjPool(int size, T t) {
+        pool = new Vector<T>() {
+        };
+        for (int i = 0; i < size; i++) {
+            pool.add(t);
+        }
+        sem = new Semaphore(size);
+    }
+
+    // 利用对象池的对象，调用func
+    R exec(Function<T, R> func) throws InterruptedException {
+        T t = null;
+        sem.acquire();
+        try {
+            t = pool.remove(0);
+            return func.apply(t);
+        } finally {
+            pool.add(t);
+            sem.release();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建对象池
+        ObjPool<Long, String> pool = new ObjPool<Long, String>(10, 2L);
+        // 通过对象池获取t，之后执行
+        pool.exec(t -> {
+            System.out.println(t);
+            return t.toString();
+        });
+    }
+}
