63ebde983825db02326d3051934a134af2c223de
mask <1429855087@qq.com>
2023-02-16 23:03:46 +0800

    ThreadPoolExecutorSubmit

---
+++ src/main/java/com/zhy/java基础/juc/threadpool/future/ThreadPoolExecutorSubmit.java
@@ -0,0 +1,19 @@
+package com.zhy.java基础.juc.threadpool.future;
+
+/**
+ * 提交Runnable任务
+ *      Future submit(Runnable task);
+ *
+ * 提交Callable任务
+ *      Future submit(Callable task);
+ *
+ * 提交Runnable任务及结果引用
+ *      Future submit(Runnable task, T result);
+ *
+ * @author zhouhongyin
+ * @since 2023/2/16 23:02
+ */
+public class ThreadPoolExecutorSubmit {
+
+
+}
