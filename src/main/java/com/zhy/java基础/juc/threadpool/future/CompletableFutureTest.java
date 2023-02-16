f3713225fa69d2fa5a9e74134f0b8646a5ded01b
mask <1429855087@qq.com>
2023-02-16 23:38:37 +0800

    CompletableFutureTest

---
+++ src/main/java/com/zhy/java基础/juc/threadpool/future/CompletableFutureTest.java
@@ -0,0 +1,20 @@
+package com.zhy.java基础.juc.threadpool.future;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * CompletableFuture
+ * @author zhouhongyin
+ * @since 2023/2/16 22:55
+ */
+public class CompletableFutureTest {
+
+    // TODO-zhouhy 2023/2/16 CompletableFuture 的使用
+    public static void main(String[] args) {
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> "1");
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> "2");
+        CompletableFuture<String> completableFuture3 = CompletableFuture.supplyAsync(() -> "3");
+        CompletableFuture.anyOf(completableFuture1, completableFuture2, completableFuture3)
+                .thenAccept(System.out::println);
+    }
+}
