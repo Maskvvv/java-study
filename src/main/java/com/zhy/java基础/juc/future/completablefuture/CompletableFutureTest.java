f92a68106673a206e5cf6421de3edf14ff716a1f
zhouhongyin <1429855087@qq.com>
2023-02-19 17:32:51 +0800

    CompletableFutureTest

--- src/main/java/com/zhy/java基础/juc/future/completablefuture/CompletableFutureTest.java
+++ src/main/java/com/zhy/java基础/juc/future/completablefuture/CompletableFutureTest.java
@@ -1,6 +1,10 @@
 package com.zhy.java基础.juc.future.completablefuture;
 
+import org.junit.Test;
+
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * CompletableFuture
@@ -16,5 +20,36 @@ public class CompletableFutureTest {
         CompletableFuture<String> completableFuture3 = CompletableFuture.supplyAsync(() -> "3");
         CompletableFuture.anyOf(completableFuture1, completableFuture2, completableFuture3)
                 .thenAccept(System.out::println);
+
+
+
+    }
+
+    @Test
+    public void test1() {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("future1完成！");
+            return "future1完成！";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("future2完成！");
+            return "future2完成！";
+        });
+
+        CompletableFuture<Void> combindFuture = CompletableFuture.allOf(future1, future2);
+
+        try {
+            combindFuture.get().var;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
