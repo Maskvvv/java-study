0e05e43c592662064b282704eaf2d92c0fd369f7
mask <1429855087@qq.com>
2023-02-25 22:07:07 +0800

    elasticsearch doc search option

---
+++ src/main/java/com/zhy/middleware/es/ESDocSearch.java
@@ -0,0 +1,406 @@
+package com.zhy.middleware.es;
+
+import com.alibaba.fastjson.JSON;
+import com.zhy.middleware.es.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.Nullable;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.FuzzyQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author zhouhongyin
+ * @since 2023/2/25 20:30
+ */
+@Slf4j
+public class ESDocSearch {
+
+    /**
+     * 全量查询
+     */
+    @Test
+    public void matchAllQuery() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 条件查询
+     */
+    @Test
+    public void termQuery() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.termQuery("age", 10));
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 分页查询
+     */
+    @Test
+    public void pageSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        sourceBuilder.from(0).size(2);
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 排序查询
+     */
+    @Test
+    public void sortSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        sourceBuilder.sort("age", SortOrder.DESC);
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 过滤查询
+     */
+    @Test
+    public void filterSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+
+        String[] includes = {"name"};
+        String[] excludes = {};
+        sourceBuilder.fetchSource(includes, excludes);
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 组合查询
+     */
+    @Test
+    public void combineSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.matchQuery("age", 10));
+        boolQueryBuilder.must(QueryBuilders.matchQuery("gender", "男"));
+
+        boolQueryBuilder.should(QueryBuilders.matchQuery("gender", "女"));
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(boolQueryBuilder);
+
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 范围查询
+     */
+    @Test
+    public void rangeSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("age");
+        rangeQueryBuilder.gte(11);
+        rangeQueryBuilder.lt(13);
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(rangeQueryBuilder);
+
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 模糊查询
+     */
+    @Test
+    public void fuzzySearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        FuzzyQueryBuilder fuzzyQueryBuilder = QueryBuilders.fuzzyQuery("name", "张").fuzziness(Fuzziness.ONE);
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(fuzzyQueryBuilder);
+
+
+        searchRequest.source(sourceBuilder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 高亮查询
+     */
+    @Test
+    public void highlightSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        FuzzyQueryBuilder fuzzyQueryBuilder = QueryBuilders.fuzzyQuery("name", "张三").fuzziness(Fuzziness.ONE);
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().query(fuzzyQueryBuilder);
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<front color='red'>");
+        highlightBuilder.postTags("</front>");
+        highlightBuilder.field("name");
+        sourceBuilder.highlighter(highlightBuilder);
+
+        searchRequest.source(sourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 聚合查询
+     */
+    @Test
+    public void aggSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        AggregationBuilder aggregationBuilder = AggregationBuilders.max("maxAge").field("age");
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().aggregation(aggregationBuilder);
+
+        searchRequest.source(sourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        for (Aggregation aggregation : response.getAggregations()) {
+            System.out.println(aggregation.getName());
+        }
+        SearchHits hits = response.getHits();
+
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+    /**
+     * 分组查询
+     */
+    @Test
+    public void groupSearch() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        SearchRequest searchRequest = new SearchRequest("user");
+
+        // 构建查询条件
+        AggregationBuilder aggregationBuilder = AggregationBuilders.terms("groupAge").field("age");
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder().aggregation(aggregationBuilder);
+
+        searchRequest.source(sourceBuilder);
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        for (Aggregation aggregation : response.getAggregations()) {
+            System.out.println(aggregation.getName());
+            System.out.println(aggregation.getMetadata());
+        }
+        SearchHits hits = response.getHits();
+
+
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit);
+        }
+
+        client.close();
+
+    }
+
+
+}
