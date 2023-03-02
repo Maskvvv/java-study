8892fcb93c8a598debf5ec470abfb7e0b0ab9255
zhouhongyin <1429855087@qq.com>
2023-03-02 21:12:25 +0800

    spEL aop log

---
+++ src/main/java/com/zhy/spring/aop/spel/MethodLog.java
@@ -0,0 +1,19 @@
+package com.zhy.spring.aop.spel;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author zhouhongyin
+ * @since 2023/3/2 17:55
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodLog {
+
+
+
+
+}
