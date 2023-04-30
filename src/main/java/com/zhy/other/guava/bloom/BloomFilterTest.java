1ccd2d4601e251860c7408ddb85592e8425b7c04
mask <1429855087@qq.com>
2023-04-30 15:10:27 +0800

    guava BloomFilter

---
+++ src/main/java/com/zhy/other/guava/bloom/BloomFilterTest.java
@@ -0,0 +1,28 @@
+package com.zhy.other.guava.bloom;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+/**
+ * @author zhouhongyin
+ * @since 2023/4/30 15:02
+ */
+public class BloomFilterTest {
+    public static void main(String[] args) {
+        // 创建布隆过滤器对象
+        BloomFilter<Integer> filter = BloomFilter.create(
+                Funnels.integerFunnel(),
+                1500,
+                0.01);
+        // 判断指定元素是否存在
+        System.out.println(filter.mightContain(1));
+        System.out.println(filter.mightContain(2));
+        // 将元素添加进布隆过滤器
+        filter.put(1);
+        filter.put(2);
+        System.out.println(filter.mightContain(1));
+        System.out.println(filter.mightContain(2));
+
+    }
+
+}
