5bb1476cf07bb11f4e27669c923ccd857150668d
mask <1429855087@qq.com>
2023-05-11 21:55:49 +0800

    comparable an comparator

--- src/main/java/com/zhy/java基础/collection/compare/MyComparable.java
+++ src/main/java/com/zhy/java基础/collection/compare/MyComparable.java
@@ -1,16 +1,23 @@
 package com.zhy.java基础.collection.compare;
 
+import org.junit.Test;
+
 import java.util.Set;
 import java.util.TreeMap;
 
 /**
+ * Comparable
+ *
  * @author zhouhongyin
  * @since 2023/5/11 21:41
  */
 public class MyComparable {
 
-    public static void main(String[] args) {
-
+    /**
+     * key 实现comparable
+     */
+    @Test
+    public void test1() {
         TreeMap<PeopleCompareModel, String> map = new TreeMap<>();
 
         map.put(new PeopleCompareModel("2", 2), "2");
@@ -19,7 +26,21 @@ public class MyComparable {
         Set<PeopleCompareModel> keySet = map.keySet();
 
         keySet.forEach(System.out::println);
+    }
 
+    /**
+     * set 构造方法使用 comparator
+     */
+    @Test
+    public void test2() {
+        TreeMap<PeopleCompareModel, String> map = new TreeMap<>(((o1, o2) -> -o1.getAge().compareTo(o2.getAge())));
+
+        map.put(new PeopleCompareModel("2", 2), "2");
+        map.put(new PeopleCompareModel("1", 1), "1");
+
+        Set<PeopleCompareModel> keySet = map.keySet();
+
+        keySet.forEach(System.out::println);
     }
 
 }
