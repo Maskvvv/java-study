99a33d8d2def3fb7e20ea39bacca5c3f2d52a04c
zhouhongyin <1429855087@qq.com>
2023-05-17 17:35:08 +0800

    TypeSafe

---
+++ src/main/java/com/zhy/other/typesafe/TypeSafeTest.java
@@ -0,0 +1,18 @@
+package com.zhy.other.typesafe;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+/**
+ * @author zhouhongyin
+ * @since 2023/5/17 16:59
+ */
+public class TypeSafeTest {
+
+    public static void main(String[] args) {
+        String configInfo = "";
+        Config load = ConfigFactory.parseString(configInfo);
+
+    }
+
+}
