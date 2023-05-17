08a8511033b70e52f074ca2e9a6689ff625f998d
zhouhongyin <1429855087@qq.com>
2023-05-17 17:35:56 +0800

    TypeSafe

--- src/main/java/com/zhy/other/typesafe/TypeSafeTest.java
+++ src/main/java/com/zhy/other/typesafe/TypeSafeTest.java
@@ -3,6 +3,8 @@ package com.zhy.other.typesafe;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.util.List;
+
 /**
  * @author zhouhongyin
  * @since 2023/5/17 16:59
@@ -12,6 +14,15 @@ public class TypeSafeTest {
     public static void main(String[] args) {
         String configInfo = "";
         Config load = ConfigFactory.parseString(configInfo);
+
+
+        String mapping = load.getString("mapping");
+        System.out.println(mapping);
+
+        List<String> like = load.getStringList("like");
+        for (String s : like) {
+            System.out.println(s);
+        }
 
     }
 
