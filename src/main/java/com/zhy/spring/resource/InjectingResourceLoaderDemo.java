b0d12754d7eb8c6be52df3a37cdc828ced3d158e
mask <1429855087@qq.com>
2023-05-31 22:42:45 +0800

    注入 ResourceLoader 对象示例

---
+++ src/main/java/com/zhy/spring/resource/InjectingResourceLoaderDemo.java
@@ -0,0 +1,53 @@
+package com.zhy.spring.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 注入 {@link ResourceLoader} 对象示例
+ *
+ * @see ResourceLoader
+ * @see Resource
+ * @see Value
+ * @see AnnotationConfigApplicationContext
+ */
+public class InjectingResourceLoaderDemo implements ResourceLoaderAware {
+
+    private ResourceLoader resourceLoader; // 方法一
+
+    @Autowired
+    private ResourceLoader autowiredResourceLoader; // 方法二
+
+    @Autowired
+    private AbstractApplicationContext applicationContext; // 方法三
+
+    @PostConstruct
+    public void init() {
+        System.out.println("resourceLoader == autowiredResourceLoader : " + (resourceLoader == autowiredResourceLoader));
+        System.out.println("resourceLoader == applicationContext : " + (resourceLoader == applicationContext));
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        // 注册当前类作为 Configuration Class
+        context.register(InjectingResourceLoaderDemo.class);
+        // 启动 Spring 应用上下文
+        context.refresh();
+        // 关闭 Spring 应用上下文
+        context.close();
+
+    }
+}
