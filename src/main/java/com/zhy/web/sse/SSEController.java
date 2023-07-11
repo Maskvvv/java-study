701fa7c04ec0f1ae3572e850fdab0f603d386e56
zhouhongyin <1429855087@qq.com>
2023-07-11 17:20:17 +0800

    server-send event

--- src/main/java/com/zhy/web/sse/SSEController.java
+++ src/main/java/com/zhy/web/sse/SSEController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
 
@@ -48,34 +49,26 @@ public class SSEController {
     }
 
 
-    @GetMapping(value = "chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public void chat(HttpServletResponse response) {
+    @GetMapping(value = "text", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public void text(HttpServletResponse response) throws IOException, InterruptedException {
         response.setContentType("text/event-stream");
         response.setCharacterEncoding("utf-8");
 
         FileReader fileReader = new FileReader("D:\\UserFiles\\桌面\\新建文本文档 (2).txt");
-        fileReader.read()
 
-        while (true ) {
-            Random r = new Random();
-            try {
-                Thread.sleep(1000);
-                PrintWriter pw = response.getWriter();
+        for (int n = fileReader.read(); n != -1; n = fileReader.read()) {
+            Thread.sleep(30);
+            PrintWriter pw = response.getWriter();
 
-
-                if (pw.checkError()) {
-                    System.out.println("客户端断开连接");
-                    return;
-                }
-
-                String s = "data:Testing 1,2,3" + r.nextInt() + "\n\n";
-                System.out.println(s);
-                pw.write(s);
-                pw.flush();
-
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (pw.checkError()) {
+                System.out.println("客户端断开连接");
+                return;
             }
+            char c = (char) n;
+            System.out.println(c);
+            pw.write(c);
+            pw.flush();
         }
+
     }
 }
