aafb418218bc9665087452dbec306e5e970ea8e4
zhouhongyin <1429855087@qq.com>
2023-07-19 15:17:07 +0800

    json类型 返回值大小的计算方式

---
+++ src/main/java/com/zhy/web/bytebit/ByteBitModel.java
@@ -0,0 +1,19 @@
+package com.zhy.web.bytebit;
+
+import lombok.Data;
+
+/**
+ * <p> </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/19 15:06
+ */
+@Data
+public class ByteBitModel {
+
+    private String string;
+
+    private Integer integer;
+
+}
+
