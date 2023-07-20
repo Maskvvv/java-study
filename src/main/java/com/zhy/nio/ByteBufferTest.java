5ed9653786ec1b2beaf1f9c7bb741e5d8c3481b4
zhouhongyin <1429855087@qq.com>
2023-07-20 10:09:44 +0800

    ByteBuffer

---
+++ src/main/java/com/zhy/nio/ByteBufferTest.java
@@ -0,0 +1,45 @@
+package com.zhy.nio;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * <p> ByteBuffer </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/20 9:37
+ */
+public class ByteBufferTest {
+
+
+    @Test
+    public void test1() {
+        URL resource = ByteBufferTest.class.getClassLoader().getResource("cp_id.txt");
+        try (FileChannel channel = new FileInputStream(resource.getFile()).getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+
+            while (channel.read(buffer) != -1) {
+
+                buffer.flip();
+
+                while (buffer.hasRemaining()) {
+                    byte b = buffer.get();
+                    System.out.print(((char) b));
+                }
+
+                buffer.clear();
+            }
+
+        } catch (IOException e) {
+
+        }
+
+    }
+
+}
