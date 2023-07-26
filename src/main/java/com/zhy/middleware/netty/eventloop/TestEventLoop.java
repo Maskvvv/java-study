ef519cb4698ef2a0243ea6c2d943d4efa10f7e48
zhouhongyin <1429855087@qq.com>
2023-07-26 21:10:52 +0800

    EventLoop

---
+++ src/main/java/com/zhy/middleware/netty/eventloop/TestEventLoop.java
@@ -0,0 +1,29 @@
+package com.zhy.middleware.netty.eventloop;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestEventLoop {
+    public static void main(String[] args) {
+        // 创建拥有两个EventLoop的NioEventLoopGroup，对应两个线程
+        EventLoopGroup group = new NioEventLoopGroup(2);
+        // 通过next方法可以获得下一个 EventLoop
+        System.out.println(group.next());
+        System.out.println(group.next());
+
+        // 通过EventLoop执行普通任务
+        group.next().execute(()->{
+            System.out.println(Thread.currentThread().getName() + " hello");
+        });
+
+        // 通过EventLoop执行定时任务
+        group.next().scheduleAtFixedRate(()->{
+            System.out.println(Thread.currentThread().getName() + " hello2");
+        }, 0, 1, TimeUnit.SECONDS);
+
+        // 优雅地关闭
+        group.shutdownGracefully();
+    }
+}
