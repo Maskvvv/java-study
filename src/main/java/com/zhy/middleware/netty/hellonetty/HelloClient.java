d9d23aae08c05b5efab13506d80d86f3ed10d7ae
zhouhongyin <1429855087@qq.com>
2023-07-26 17:30:45 +0800

    hello netty

---
+++ src/main/java/com/zhy/middleware/netty/hellonetty/HelloClient.java
@@ -0,0 +1,43 @@
+package com.zhy.middleware.netty.hellonetty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * <p> netty 客户端 </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/26 17:09
+ */
+public class HelloClient {
+    public static void main(String[] args) throws InterruptedException {
+        new Bootstrap()
+                .group(new NioEventLoopGroup())
+                // 选择客户 Socket 实现类，NioSocketChannel 表示基于 NIO 的客户端实现
+                .channel(NioSocketChannel.class)
+                // ChannelInitializer 处理器（仅执行一次）
+                // 它的作用是待客户端SocketChannel建立连接后，执行initChannel以便添加更多的处理器
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel channel) throws Exception {
+                        // 消息会经过通道 handler 处理，这里是将 String => ByteBuf 编码发出
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 指定要连接的服务器和端口
+                .connect(new InetSocketAddress("localhost", 8080))
+                // Netty 中很多方法都是异步的，如 connect
+                // 这时需要使用 sync 方法等待 connect 建立连接完毕
+                .sync()
+                // 获取 channel 对象，它即为通道抽象，可以进行数据读写操作
+                .channel()
+                // 写入消息并清空缓冲区
+                .writeAndFlush("hello world");
+    }
+}
---
+++ src/main/java/com/zhy/middleware/netty/hellonetty/HelloServer.java
@@ -0,0 +1,45 @@
+package com.zhy.middleware.netty.hellonetty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * <p> netty 服务端 </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/26 17:07
+ */
+public class HelloServer {
+    public static void main(String[] args) {
+        // 1、启动器，负责装配netty组件，启动服务器
+        new ServerBootstrap()
+                // 2、创建 NioEventLoopGroup，可以简单理解为 线程池 + Selector
+                .group(new NioEventLoopGroup())
+                // 3、选择服务器的 ServerSocketChannel 实现
+                .channel(NioServerSocketChannel.class)
+                // 4、child 负责处理读写，该方法决定了 child 执行哪些操作
+                // ChannelInitializer 处理器（仅执行一次）
+                // 它的作用是待客户端SocketChannel建立连接后，执行initChannel以便添加更多的处理器
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        // 5、SocketChannel的处理器，使用StringDecoder解码，ByteBuf=>String
+                        nioSocketChannel.pipeline().addLast(new StringDecoder());
+                        // 6、SocketChannel的业务处理，使用上一个处理器的处理结果
+                        nioSocketChannel.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+                                System.out.println(s);
+                            }
+                        });
+                    }
+                    // 7、ServerSocketChannel绑定8080端口
+                }).bind(8080);
+    }
+}
