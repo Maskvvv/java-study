d4a331ebcbf64bb2d65c6d60198fe1c00810a437
mask <1429855087@qq.com>
2023-07-29 17:48:51 +0800

    netty 析 Http 协议

---
+++ src/main/java/com/zhy/middleware/netty/protocoldesign/HttpProtocol.java
@@ -0,0 +1,78 @@
+package com.zhy.middleware.netty.protocoldesign;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p> 解析 Http 协议 </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/29 15:22
+ */
+@Slf4j
+public class HttpProtocol {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        new ServerBootstrap()
+                .group(group)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+                                System.out.println("Http request message > \n" + ((ByteBuf) msg).toString(Charset.defaultCharset()));
+                                super.channelRead(ctx, msg);
+                            }
+                        });
+                        // 作为服务器，使用 HttpServerCodec 作为编码器与解码器
+                        ch.pipeline().addLast(new HttpServerCodec());
+                        // 服务器只处理HTTPRequest
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<HttpRequest>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, HttpRequest msg) {
+                                // 获得请求uri
+                                log.debug(msg.uri());
+
+                                // 获得完整响应，设置版本号与状态码
+                                DefaultFullHttpResponse response = new DefaultFullHttpResponse(msg.protocolVersion(), HttpResponseStatus.OK);
+
+                                // 设置响应内容
+                                byte[] bytes = "<h1>Hello, World!</h1>".getBytes(StandardCharsets.UTF_8);
+                                // 设置响应体长度，避免浏览器一直接收响应内容
+                                response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, bytes.length);
+                                response.headers().set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON);
+                                // 设置响应体
+                                response.content().writeBytes(bytes);
+
+                                // 写回响应
+                                ctx.writeAndFlush(response);
+                            }
+                        });
+                    }
+                })
+                .bind(8080);
+    }
+}
