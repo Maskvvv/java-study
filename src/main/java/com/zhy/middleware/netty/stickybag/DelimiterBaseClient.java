d925c31882651c804c4f7a17f330c7730f6a2c53
mask <1429855087@qq.com>
2023-07-29 14:28:51 +0800

    netty 自定义分隔符行解码器

---
+++ src/main/java/com/zhy/middleware/netty/stickybag/DelimiterBaseClient.java
@@ -0,0 +1,70 @@
+package com.zhy.middleware.netty.stickybag;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+/**
+ * <p> netty 自定义分隔符行解码器  </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/7/28 17:34
+ */
+@Slf4j
+public class DelimiterBaseClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    log.debug("connected...");
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            log.debug("sending...");
+                            // 约定最大长度为 64
+                            // 被发送的数据
+                            char c = 'a';
+                            for (int i = 0; i < 10; i++) {
+                                ByteBuf buffer = ctx.alloc().buffer();
+                                // 生成长度为0~62的数据
+                                Random random = new Random();
+                                StringBuilder sb = new StringBuilder();
+                                for (int j = 0; j < (random.nextInt(64)) + 1; j++) {
+                                    sb.append(c);
+                                }
+                                // 数据以 \n 结尾
+                                sb.append("\\c");
+                                buffer.writeBytes(sb.toString().getBytes(StandardCharsets.UTF_8));
+                                c++;
+                                // 将数据发送给服务器
+                                ctx.writeAndFlush(buffer);
+                            }
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
