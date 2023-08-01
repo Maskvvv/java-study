30296c8694d11c7a84cb607949a1cbe9dffb82a3
zhouhongyin <1429855087@qq.com>
2023-08-01 13:36:03 +0800

    caffeine cache and async cache

--- src/main/java/com/zhy/other/guava/cache/CaffeineTest.java
+++ src/main/java/com/zhy/other/guava/cache/CaffeineTest.java
@@ -4,9 +4,13 @@ import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.zhy.model.git.entity.People;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -19,9 +23,11 @@ public class CaffeineTest {
 
     /**
      * 手动加载
+     * <P>
+     * LoadingCache key 不能为空
      */
     @Test
-    public void test1() {
+    public void loadingCacheNullKey() {
         LoadingCache<String, Object> cache = Caffeine.newBuilder()
                 .maximumSize(100)
                 .expireAfterWrite(10, TimeUnit.MINUTES)
@@ -39,20 +45,70 @@ public class CaffeineTest {
     }
 
     /**
-     * 异步加载
+     * 手动加载
+     * <P>
+     * LoadingCache value 返回空即未命中缓存，所以需要注意缓存穿透
      */
-    public CompletableFuture<Object> asyncOperator(String key) {
-        AsyncLoadingCache<String, Object> cache = Caffeine.newBuilder()
-                .maximumSize(100)
-                .expireAfterWrite(1, TimeUnit.MINUTES)
-                .buildAsync(k -> setAsyncValue(key).get());
+    @Test
+    public void loadingCacheNullValue() {
+        LoadingCache<String, Object> cache = Caffeine.newBuilder()
+                .maximumSize(100).recordStats()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(k -> {
+                    System.out.println("generate value");
+                    return null;
+                });
 
-        CompletableFuture<Object> objectCompletableFuture = cache.get(key);
-        return objectCompletableFuture;
+        cache.get("key1");
+        cache.get("key1");
+        cache.get("key1");
+
+        System.out.println(cache.stats());
     }
 
-    public CompletableFuture<Object> setAsyncValue(String key) {
+    /**
+     * 异步加载
+     */
+    @Test
+    public void asyncOperator() throws ExecutionException, InterruptedException {
+        DefaultThreadFactory defaultThreadFactory = new DefaultThreadFactory("mypool");
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        AsyncLoadingCache<String, String> cache = Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(1, TimeUnit.MINUTES)
+                .executor(threadPool)
+                //.buildAsync(k -> setAsyncValue(k).get());
+                .buildAsync((k, executor) -> {
+                    System.out.println("async executor current thread: " + Thread.currentThread().getName());
+
+                    return CompletableFuture.supplyAsync(() -> {
+                        System.out.println("supplyAsync executor current thread: " + Thread.currentThread().getName());
+                       return  "this key is " + k;
+                    }, threadPool);
+                });
+
+        CompletableFuture<String> future = cache.get("key");
+
+        future.thenAccept(v -> {
+            System.out.println("thenAccept current thread: " + Thread.currentThread().getName());
+        });
+
+        System.out.println("main current thread: " + Thread.currentThread().getName());
+        System.out.println(future.get());
+
+
+    }
+
+    public CompletableFuture<String> setAsyncValue(String key) {
         return CompletableFuture.supplyAsync(() -> {
+            System.out.println("current thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             return key + "value";
         });
     }
