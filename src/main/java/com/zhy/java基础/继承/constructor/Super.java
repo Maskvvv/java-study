5ad1f4d44fecd0bb178dd322ccf9a565b2d106bb
Maskvvv <1429855087@qq.com>
2023-10-08 11:20:11 +0800

    inherit class

---
+++ src/main/java/com/zhy/java基础/继承/constructor/Super.java
@@ -0,0 +1,15 @@
+package com.zhy.java基础.继承.constructor;
+
+/**
+ * <p> </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/10/8 10:50
+ */
+public class Super {
+    private String name;
+
+    public Super(String name) {
+        this.name = name;
+    }
+}
