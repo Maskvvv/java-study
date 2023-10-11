77fc1952f89bf374afd15c45dbcb13025bdc6eb9
Maskvvv <1429855087@qq.com>
2023-10-11 09:53:59 +0800

    MethodArea OOM(JVM)

---
+++ src/main/java/com/zhy/java基础/jvm/oom/JavaMethodAreaOOM.java
@@ -0,0 +1,34 @@
+package com.zhy.java基础.jvm.oom;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * <p> MethodArea OOM </p>
+ *
+ * <P>JDK7: {@code VM Args: -XX:PermSize=10M -XX:MaxPermSize=10M}</P>
+ * <P>JDK8+: {@code VM Args: -XX:MaxMetaspaceSize=10M}</P>
+ *
+ * @author zhouhongyin
+ */
+public class JavaMethodAreaOOM {
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                    return proxy.invokeSuper(obj, args);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject {
+    }
+}
