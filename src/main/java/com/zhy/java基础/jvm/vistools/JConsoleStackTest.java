18c47ea823ccc806b945ccf0230487275bdd0495
mask <1429855087@qq.com>
2023-10-14 18:44:06 +0800

    JConsole Test(JVM)

---
+++ src/main/java/com/zhy/java基础/jvm/vistools/JConsoleStackTest.java
@@ -0,0 +1,56 @@
+package com.zhy.java基础.jvm.vistools;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * <p> JConsole Stack Test </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/10/14 18:18
+ */
+public class JConsoleStackTest {
+
+    /**
+     * 线程死循环演示
+     */
+    public static void createBusyThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) // 第41行
+                    ;
+            }
+        }, "testBusyThread");
+        thread.start();
+    }
+
+    /**
+     * 线程锁等待演示
+     */
+    public static void createLockThread(final Object lock) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "testLockThread");
+        thread.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        createBusyThread();
+        br.readLine();
+        Object obj = new Object();
+        createLockThread(obj);
+    }
+
+}
