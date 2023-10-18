2b2be9fe540d868a9afc8eccfe07793234d39441
Maskvvv <1429855087@qq.com>
2023-10-18 13:47:24 +0800

    Constants pool(Jvm)

---
+++ src/main/java/com/zhy/java基础/jvm/constantspool/TestClass.java
@@ -0,0 +1,15 @@
+package com.zhy.java基础.jvm.constantspool;
+
+/**
+ * <p> Constants pool </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/10/18 13:46
+ */
+public class TestClass {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
