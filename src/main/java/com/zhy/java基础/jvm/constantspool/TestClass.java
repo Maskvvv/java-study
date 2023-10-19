733bf76853c51faef3420383b3d7ca1452cdccd7
mask <1429855087@qq.com>
2023-10-19 23:14:01 +0800

    Constants pool(JVM)

--- src/main/java/com/zhy/java基础/jvm/constantspool/TestClass.java
+++ src/main/java/com/zhy/java基础/jvm/constantspool/TestClass.java
@@ -10,6 +10,16 @@ public class TestClass {
     private int m;
 
     public int inc() {
-        return m + 1;
+        int x;
+        try {
+            x = 1;
+            return x;
+        } catch (Exception e) {
+            x = 2;
+            return x;
+        } finally {
+            x = 3;
+        }
+
     }
 }
