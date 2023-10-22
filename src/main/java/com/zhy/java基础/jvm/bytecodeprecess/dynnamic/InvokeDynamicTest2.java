909d49642ea27273d160f345bf5a5c8f450b66e0
mask <1429855087@qq.com>
2023-10-22 23:34:07 +0800

    InvokeDynamicTest(JVM)

---
+++ src/main/java/com/zhy/java基础/jvm/bytecodeprecess/dynnamic/InvokeDynamicTest2.java
@@ -0,0 +1,45 @@
+package com.zhy.java基础.jvm.bytecodeprecess.dynnamic;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
+
+/**
+ * <p> InvokeDynamicTest </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/10/22 23:29
+ */
+public class InvokeDynamicTest2 {
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                Field lookupImpl = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+                lookupImpl.setAccessible(true);
+                MethodHandle mh = ((MethodHandles.Lookup) lookupImpl.get(null))
+                        .findSpecial(GrandFather.class, "thinking", mt, GrandFather.class);
+                mh.invoke(this);
+
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        (new InvokeDynamicTest2().new Son()).thinking();
+    }
+}
