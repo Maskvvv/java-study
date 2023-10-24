bdf1187b47ff418c6cb76da12023bb4807808f82
mask <1429855087@qq.com>
2023-10-24 23:21:58 +0800

    DynamicProxyTest(JVM)

--- src/main/java/com/zhy/java基础/jvm/bytecodeprecess/DynamicProxyTest.java
+++ src/main/java/com/zhy/java基础/jvm/bytecodeprecess/DynamicProxyTest.java
@@ -44,6 +44,7 @@ public class DynamicProxyTest {
 
 
     public static void main(String[] args) {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         IHello hello = (IHello) new DynamicProxy().bind(new Hello());
         hello.sayHello();
     }
