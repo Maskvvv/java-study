ce5eeb15d5e2b059b6a37e330ac96d2e0aebfc75
Maskvvv <1429855087@qq.com>
2023-10-26 14:58:05 +0800

    File list filter(File)

--- src/main/java/com/zhy/java基础/IO/file/IOFileDemo1.java
+++ src/main/java/com/zhy/java基础/IO/file/IOFileDemo1.java
@@ -1,10 +1,19 @@
 package com.zhy.java基础.IO.file;
 
 
+import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
 
+/**
+ * <p> File API test </p>
+ *
+ * @author zhouhongyin
+ * @since 2023/10/26 14:51
+ */
 public class IOFileDemo1 {
     public static void main(String[] args) throws IOException {
         File file = new File("./");
@@ -40,5 +49,23 @@ public class IOFileDemo1 {
         System.out.println(list.toString());
 
 
+    }
+
+    /**
+     * File list filter
+     */
+    @Test
+    public void test1() {
+
+        File file = new File("D:\\UserFiles\\桌面");
+
+        //过滤不想要的文件和目录
+        String[] list = file.list(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith("报名信息_20231026");
+            }
+        });
+
+        System.out.println(Arrays.toString(list));
     }
 }
