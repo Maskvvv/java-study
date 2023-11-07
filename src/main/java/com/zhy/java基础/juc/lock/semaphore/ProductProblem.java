b52fd1f630e0fb1f6decf61f67519bf6eda9c102
Maskvvv <1429855087@qq.com>
2023-11-07 11:30:10 +0800

    producer consumer problem (juc)

--- src/main/java/com/zhy/java基础/juc/lock/semaphore/ProductProblem.java
+++ src/main/java/com/zhy/java基础/juc/lock/semaphore/ProductProblem.java
@@ -1,7 +1,11 @@
 package com.zhy.java基础.juc.lock.semaphore;
 
-import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * <p> producer consumer problem </p>
@@ -10,35 +14,87 @@ import java.util.concurrent.Semaphore;
  * @since 2023/11/7 10:40
  */
 public class ProductProblem {
+    public static final AtomicInteger message = new AtomicInteger();
 
-    public static final Semaphore SEMAPHORE = new Semaphore(0);
+    // 互斥信号量
+    public static final Semaphore mutex = new Semaphore(1);
+    // 同步信号量 表示空闲缓冲区的数量
+    public static final Semaphore empty = new Semaphore(10);
+    // 同步信号量 表示产品的数量
+    public static final Semaphore full = new Semaphore(0);
+
+    public static final Deque<String> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = new Thread(() -> {
-            System.out.println("try acquire");
+        List<Thread> threads = new ArrayList<>();
 
+        int producerCount = 0;
+        int consumerCount = 0;
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                threads.add(new Thread(new Producer(), "producer" + producerCount++));
+            } else {
+                threads.add(new Thread(new Consumer(), "consumer" + consumerCount++));
+            }
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("end");
+
+    }
+
+
+    public static class Producer implements Runnable {
+        @Override
+        public void run() {
             try {
-                SEMAPHORE.acquire();
+                for (int i = 0; i < 100; i++) {
+
+                    empty.acquire();
+                    mutex.acquire();
+
+
+                    System.out.println("生产者-" + Thread.currentThread().getName() + "-生产");
+                    queue.addLast(String.valueOf(message.getAndIncrement()));
+
+                    full.release();
+                    mutex.release();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static class Consumer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 100; i++) {
+
+                    full.acquire();
+                    mutex.acquire();
+
+                    System.out.println("消费者-" + Thread.currentThread().getName() + "-消费: " + queue.removeFirst());
+
+                    empty.release();
+                    mutex.release();
+                }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            System.out.println("acquire success");
 
-        });
-        thread.start();
-
-        Scanner scanner = new Scanner(System.in);
-        String next = scanner.next();
-
-        SEMAPHORE.release();
-
-        thread.join();
-        System.out.println("thread success");
-
+        }
     }
-
 
 
 }
