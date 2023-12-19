e0e6df3903ad5f9b7fd69c5e8a2d3587226a2bdc
Maskvvv <1429855087@qq.com>
2023-12-19 11:01:21 +0800

    通过 properties 配置元信息 (spring bean)

---
+++ src/main/java/com/zhy/spring/beanlifecycle/BeanMetadataConfigurationDemo.java
@@ -0,0 +1,33 @@
+package com.zhy.spring.beanlifecycle;
+
+import com.zhy.spring.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+
+/**
+ * Bean 元信息配置示例
+ * <p>
+ * 通过 properties 配置元信息
+ */
+public class BeanMetadataConfigurationDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 实例化基于 Properties 资源 BeanDefinitionReader
+        PropertiesBeanDefinitionReader beanDefinitionReader = new PropertiesBeanDefinitionReader(beanFactory);
+        String location = "META-INF/user.properties";
+        // 基于 ClassPath 加载 properties 资源
+        Resource resource = new ClassPathResource(location);
+        // 指定字符编码 UTF-8
+        EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
+        int beanNumbers = beanDefinitionReader.loadBeanDefinitions(encodedResource);
+        System.out.println("已加载 BeanDefinition 数量：" + beanNumbers);
+        // 通过 Bean Id 和类型进行依赖查找
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+    }
+
+}
