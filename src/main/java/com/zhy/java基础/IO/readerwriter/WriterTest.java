2701d2ab9ad1bea7a5ed26d44e8b0719b8b508aa
Maskvvv <1429855087@qq.com>
2024-01-02 18:00:10 +0800

    FileWriter append (Java IO)

--- src/main/java/com/zhy/java基础/IO/readerwriter/WriterTest.java
+++ src/main/java/com/zhy/java基础/IO/readerwriter/WriterTest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 /**
+ * <p> Writer </p>
+ *
  * @author zhouhongyin
  * @since 2022/6/27 22:14
  */
@@ -24,14 +26,14 @@ public class WriterTest {
 
     @Test
     public void test1() throws IOException {
-        FileWriter fileWriter = new FileWriter("D:\\code\\java\\java-study\\src\\main\\java\\com\\zhy\\java基础\\IO\\readerwriter\\a.txt");
+        FileWriter fileWriter = new FileWriter("D:\\UserFiles\\桌面\\a.txt", true);
 
-        //fileWriter.write("aaaa");
-        //fileWriter.write("bbb");
-        //fileWriter.write("ccc");
+        fileWriter.write("aaaa");
+        fileWriter.write("bbb");
+        fileWriter.write("ccc");
         fileWriter.append("aaaa\n");
         fileWriter.append("bbbb");
-        fileWriter.append("ccc", 1, 2);
+        //fileWriter.append("ccc", 1, 2);
         fileWriter.close();
     }
 }
