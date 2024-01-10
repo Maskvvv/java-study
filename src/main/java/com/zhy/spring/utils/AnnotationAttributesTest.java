65e9ee8b83c0bc576f1e3474b219318b39cc9069
Maskvvv <1429855087@qq.com>
2024-01-10 14:33:05 +0800

    AnnotationConfigUtils test (Spring utils)

---
+++ src/main/java/com/zhy/spring/utils/AnnotationAttributesTest.java
@@ -0,0 +1,17 @@
+package com.zhy.spring.utils;
+
+/**
+ * <p> AnnotationConfigUtils test </p>
+ *
+ * @author zhouhongyin
+ * @since 2024/1/10 14:30
+ */
+public class AnnotationAttributesTest {
+
+    public static void main(String[] args) {
+
+        //AnnotationMetadata importingClassMetadata = new StandardAnnotationMetadata();
+        //AnnotationAttributes enableAspectJAutoProxy =
+        //        AnnotationConfigUtils.attributesFor(importingClassMetadata, EnableAspectJAutoProxy.class);
+    }
+}
