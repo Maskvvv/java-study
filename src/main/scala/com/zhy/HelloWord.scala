a0ad23fa95a96b7ed226bb47a403f325edd4dd5f
mask <1429855087@qq.com>
2024-04-02 18:06:28 +0800

    hello scala (scala study)

---
+++ src/main/scala/com/zhy/HelloWord.scala
@@ -0,0 +1,19 @@
+package com.zhy
+
+/**
+ *
+ * @author zhouhongyin
+ * @since 2024/4/2 17:38
+ */
+object HelloWord {
+
+  var int: Integer = 1
+
+  def main(args: Array[String]): Unit = {
+
+    println("hello word")
+
+    println(int)
+  }
+
+}
