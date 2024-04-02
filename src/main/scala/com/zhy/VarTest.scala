ef92464c4809c797bc9e8434d2c24600ac8465cd
mask <1429855087@qq.com>
2024-04-02 21:35:20 +0800

    Scala 变量 (scala study)

---
+++ src/main/scala/com/zhy/VarTest.scala
@@ -0,0 +1,31 @@
+package com.zhy
+
+/**
+ * Scala 变量
+ *
+ * @author zhouhongyin
+ * @since 2024/4/2 21:31
+ */
+object VarTest {
+
+
+  def main(args: Array[String]): Unit = {
+    // var 变量
+    var i: Int = 1;
+
+    // val 常量
+    val j: Int = 2;
+
+    // 类型推断
+    val myVar = 10
+    val myVal = "Hello, Scala!"
+
+    // Scala 支持多个变量的声明
+    val xmax, ymax = 100 // xmax, ymax都声明为100
+
+
+    val pa = (40, "Foo")
+
+  }
+
+}
